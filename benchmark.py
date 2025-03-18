fb7af822798d74a8a12cfb1eb24c297e361333ce
Zingo21 <kamparassignar@gmail.com>
2025-03-18 16:25:42 +0100

    Initial commit

---
+++ benchmark.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+import time
+from turtle import update
+from unittest import result
+import psutil
+import cupy as cp
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+# save results in lists
+cpu_results = []
+ram_results = []
+gpu_results = []
+
+def benchmark_cpu():
+    start = time.time()
+    sum([i**2 for i in range(10**6)])
+    end = time.time()
+    cpu_time = end - start
+    cpu_results.append(cpu_time)
+    update_plot()
+    result_label.config(text=f"CPU-test: {cpu_time:.4f} sec")
+
+    
